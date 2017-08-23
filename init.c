8b43496e9d7c9fab4ab97d91f16c8850e0d32087
sararuiz97 <sarilu97@hotmail.com>
2017-08-23 18:42:29 -0500

    Cambiar mensaje de sh

--- init.c
+++ init.c
@@ -20,7 +20,7 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "Bienvenidos chavos\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
