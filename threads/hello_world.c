31a462372f0cd86d026ed43c20e6b4b2bd3d8309
sararuiz97 <sarilu97@hotmail.com>
2017-10-04 19:17:12 -0500

    Agregar semaforo

--- threads/hello_world.c
+++ threads/hello_world.c
@@ -1,20 +1,33 @@
 #include <pthread.h>
- #include <stdio.h>
- #define NUM_THREADS     1000
+#include <stdio.h>
+#include <unistd.h>
+#include <semaphore.h>
 
- void *PrintHello(void *threadid)
- {
+#define NUM_THREADS     10
+
+
+ long saldo;
+ sem_t mutex;
+ void *PrintHello(void *threadid) {
     long tid;
+    long lsaldo;
     tid = (long)threadid;
     printf("Hello World! It's me, thread #%ld!\n", tid);
+    sem_wait(&mutex);
+    lsaldo = saldo;
+    sleep(1); //Forzando a perder el CPU
+    lsaldo += 1;
+    saldo = lsaldo;
+    sem_post(&mutex);
     pthread_exit(NULL);
  }
 
- int main (int argc, char *argv[])
- {
+ int main (int argc, char *argv[]) {
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
+    saldo = 0;
+    sem_init(&mutex, 0, 1);
     for(t=0; t<NUM_THREADS; t++){
        printf("In main: creating thread %ld\n", t);
        rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
@@ -23,6 +36,12 @@
           exit(-1);
        }
     }
+
+    for(t=0; t<NUM_THREADS; t++){
+      pthread_join(threads[t], NULL);
+    }
+
+    printf("El valor del saldo es %ld\n", saldo);
 
     /* Last thing that main() should do */
     pthread_exit(NULL);
