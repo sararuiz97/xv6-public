e3803471be6f1c33167489b01aa5eb020eae4626
sararuiz97 <sarilu97@hotmail.com>
2017-10-04 20:40:22 -0500

    Agregamos sillas

--- threads/phil.c
+++ threads/phil.c
@@ -7,8 +7,10 @@
 
 #define NUM_PHIL    5
 #define NUM_FORK    5
+#define NUM_CHAIRS  NUM_FORK/2
 
  sem_t forks[NUM_FORK];
+ sem_t chairs; 
 
  int getRand(float max){
   return 1 + (rand() * max) / RAND_MAX;
@@ -28,19 +30,14 @@
     while(1){
       printf("Im a phil %ld, thinking \n", tid );
       thinking();
-      if(tid % 2 == 0){
-        sem_wait(&forks[tid]);
-        sem_wait(&forks[(tid + 1) % NUM_PHIL]);
-      } else {
-        sem_wait(&forks[(tid + 1) % NUM_PHIL]);
-        sem_wait(&forks[tid]);
-      
-      }
-      
+      sem_wait(&chairs);
+      sem_wait(&forks[tid]);
+      sem_wait(&forks[(tid + 1) % NUM_PHIL]);
       printf("Im a phil %ld, eating \n", tid );
       eating();
       sem_post(&forks[tid]);
       sem_post(&forks[(tid + 1) % NUM_PHIL]);
+      sem_post(&chairs);
     }
     pthread_exit(NULL);
   }
@@ -55,6 +52,7 @@
       sem_init(&forks[t],0,1);
     }
 
+    sem_init(&chairs, 0, NUM_CHAIRS);
     for(t=0; t<NUM_PHIL; t++){
        rc = pthread_create(&philosophers[t], NULL, philLive, (void *)t);
        if (rc){
